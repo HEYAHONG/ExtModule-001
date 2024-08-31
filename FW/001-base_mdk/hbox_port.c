c2c95e8b4dc907e4f6f5e6c396d3956ffc76259f
HEYAHONG <2229388563@qq.com>
2024-08-31 14:09:06 +0800

    通过开关中断实现加锁

--- FW/001-base_mdk/hbox_port.c
+++ FW/001-base_mdk/hbox_port.c
@@ -8,17 +8,26 @@ hdefaults_tick_t HDEFAULTS_TICK_GET()
 }
 #endif
 
+static int lock_nested=0;
 #ifdef HDEFAULTS_MUTEX_LOCK
 void HDEFAULTS_MUTEX_LOCK()
 {
-
+    if(lock_nested==0)
+    {
+        __disable_irq();
+    }
+    lock_nested++;
 }
 #endif
 
 #ifdef HDEFAULTS_MUTEX_UNLOCK
 void HDEFAULTS_MUTEX_UNLOCK()
 {
-
+    lock_nested--;
+    if(lock_nested==0)
+    {
+        __enable_irq();
+    }
 }
 #endif
 
